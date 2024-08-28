018f274c5fcc2cd4a500be532b5279f4f10b35ef
BLack-M7 <michaelnyanteh7@gmail.com>
2024-08-28 22:59:12 +0000

    first commit

---
+++ profile paage.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>SocialConnect. | Profile</title>
+        <style>
+            #bar1{
+                height:15px;
+                background-color: aliceblue;
+            }
+            #bar2{
+                height:50px;
+                background-color:#3c5a98;
+                color:#d9dfeb;
+            }
+            #bar3{
+                width:1200px;
+                margin:auto;
+                font-size:x-large;
+                padding:10px;
+                font-style: oblique;
+            }
+            #search{
+                width:310px;
+                padding:4px;
+                border-radius: 3px;
+                border:none;
+                border-color: bisque;
+            }
+        </style>
+    </head>
+    <body>
+        <div id ="bar1">
+        </div> 
+        <div id="bar2">
+            <div id ="bar3">
+                SocialConnect.&nbsp<input type="text" id="search">
+            </div>
+        </div>
+    </body>
+</html>
