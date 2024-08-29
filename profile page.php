dd2f1df472753a4f82f59706d41e1d3e76b9bb64
BLack-M7 <michaelnyanteh7@gmail.com>
2024-08-29 22:37:03 +0000

    l changed the search button image and added a profile pic besides the search box

--- profile page.php
+++ profile page.php
@@ -26,7 +26,7 @@
                 border-radius: 3px;
                 border:none;
                 border-color: bisque;
-                background-image: url("search.png");
+                background-image: url("search3.png");
                 background-repeat:no-repeat;
                 background-position: right;
             }
@@ -37,6 +37,13 @@
                 font-size:16px;
                 opacity:45%;
             }
+            #pic{
+                width: 50px;
+                float: right;
+                height:40px;
+                margin-top: -8px;
+                padding:4px;
+            }
         </style>
     </head>
     <body>
@@ -44,7 +51,8 @@
         </div> 
         <div id="bar2">
             <div id ="bar3">
-                SocialConnect.&nbsp<input type="text" id="search" placeholder="Search for friends" id="words">
+                SocialConnect.&nbsp<input type="text" id="search" placeholder="Search for friends">
+                <img id="pic" src="pic3.jpg">
             </div>
         </div>
     </body>
